b099581cfe11037394ce5f8f5867ccccf14280cf
de-mark <data.engineer.mark@gmail.com>
2023-05-22 14:43:33 -0700

    Hooked on to network and subnet values

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,27 @@
+const networkComponent = document.getElementById("networkEntry");
+const dropdownComponent = document.getElementById("subnetDropdown");
+
+
+
 const createSubnetDropdown = () => {
+    dropdownComponent.innerHTML = ""
+
     for (let i = 30; i >= 0; i--){
-        document.getElementById("subnetDropdown").innerHTML += `
+        dropdownComponent.innerHTML += `
         <option value="${i}">/${i}</option>
         `;
         //document.getElementById("exampleSubnetMask")
     }
+    dropdownComponent.value = "30";
 }
+
+const processSubnet = () => {
+    let currentNetwork = networkComponent.value;
+    let currentSubnet = dropdownComponent.value;
+
+        alert(`NETWORK - ${currentNetwork} SUBNET - ${currentSubnet}`);
+}
+
+
 
 createSubnetDropdown();
