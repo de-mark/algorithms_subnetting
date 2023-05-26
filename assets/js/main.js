15aa50aae1f3c2b24dd4abb80c6f42614c32396f
de-mark <data.engineer.mark@gmail.com>
2023-05-26 12:00:15 -0700

    Fixed error in computation for total and useable hosts

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,21 +48,54 @@ const displaySubnetMask = () => {
 }
 
 
+const addBinary = (firstAddend, secondAddend) => {
+    let binSum = (Number(parseInt(firstAddend, 2)) + Number(parseInt(secondAddend, 2))).toString(2);
+
+    console.log(firstAddend, parseInt(firstAddend, 2));
+    console.log(secondAddend, parseInt(secondAddend, 2));
+
+    let zeroPaddedSum = "";
+
+    for (let i = 0; i < 32 - binSum.length; i++){
+        zeroPaddedSum += "0";
+    }
+
+    // 0000 0000.0000 0000.0000 0110.0000 1000
+
+    zeroPaddedSum += binSum;
+
+    console.log(zeroPaddedSum);
+
+    let newOctetsToParse = [];
+    for (let i=0; i < 32; i += 8){
+        newOctetsToParse.push(zeroPaddedSum.slice(i,i+8));
+    }
+
+    return newOctetsToParse.map((octet) => parseInt(octet, 2)).join(".");
+}
+
+
 const processSubnet = () => {
     let currentNetwork = networkComponent.value;
     let currentSubnet = dropdownComponent.value;
 
-    let nTotalHosts = 32-currentSubnet;
+    let nTotalHosts = 2**(32-currentSubnet);
     let nUseableHosts = nTotalHosts - 2;
 
-    let binaryNetwork = currentNetwork.split(".").map((octet) => parseInt(octet).toString(2));
+    let binaryNetwork = currentNetwork.split(".").map((octet) => parseInt(octet).toString(2)).join('');
+    console.log(currentNetwork);
+    console.log(binaryNetwork);
+
+    let binFirstHost = addBinary(binaryNetwork, 1);
+    let binLastHost = addBinary(binaryNetwork, nUseableHosts.toString(2));
+    let binBroadcast = addBinary(binaryNetwork, nTotalHosts.toString(2));
 
     document.getElementById("cell_network").innerHTML = currentNetwork;
-    document.getElementById("cell_totalhosts").innerHTML = nTotalHosts;
-    document.getElementById("cell_numhosts").innerHTML = nUseableHosts;
-    //document.getElementById("cell_firsthost").innerHTML = 
-    // document.getElementById("cell_lasthost").innerHTML = 
-    // document.getElementById("cell_broadcast").innerHTML =
+    document.getElementById("cell_totalhosts").innerHTML = nTotalHosts.toLocaleString();
+    document.getElementById("cell_numhosts").innerHTML = nUseableHosts.toLocaleString();
+    document.getElementById("cell_firsthost").innerHTML = binFirstHost;
+    document.getElementById("cell_lasthost").innerHTML = binLastHost;
+    document.getElementById("cell_broadcast").innerHTML = binBroadcast;
     // document.getElementById("cell_nextsub").innerHTML =
 }
 
